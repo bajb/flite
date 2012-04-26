6faf12402bfb3630af8bac257688e02d5409acdc
brooke.bryan <brooke.bryan@7a0bffa2-0bca-f930-4f03-409d7255b43b>
2012-04-26 20:33:08 +0000

    git-svn-id: https://php-flite.googlecode.com/svn/trunk@115 7a0bffa2-0bca-f930-4f03-409d7255b43b

--- php-flite/boot.php
+++ php-flite/boot.php
@@ -138,21 +138,7 @@ class FliteBase extends FliteConfig
 
         if($this->start_session && $this->GetConfig('is_web'))
         {
-            function flite_session_start()
-            {
-                if(isset($_COOKIE['PHPSESSID'])) $sessid = $_COOKIE['PHPSESSID'];
-                else if(isset($_GET['PHPSESSID'])) $sessid = $_GET['PHPSESSID'];
-                else
-                {
-                    session_start();
-                    return false;
-                }
-
-                if (!preg_match('/^[a-z0-9]{32}$/', $sessid)) return false;
-                session_start();
-                return true;
-            }
-            flite_session_start();
+            session_start();
         }
 
         spl_autoload_register(array($this, 'Loader'));
