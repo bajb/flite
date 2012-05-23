7f7536f3246c32d94fd5efd506e3131f7c2e77f7
brooke.bryan <brooke.bryan@7a0bffa2-0bca-f930-4f03-409d7255b43b>
2012-05-23 20:37:45 +0000

    git-svn-id: https://php-flite.googlecode.com/svn/trunk@134 7a0bffa2-0bca-f930-4f03-409d7255b43b

--- php-flite/core.lib/cassandra.php
+++ php-flite/core.lib/cassandra.php
@@ -1,6 +1,7 @@
 <?php
 
 use phpcassa\ColumnFamily;
+use phpcassa\SuperColumnFamily;
 use phpcassa\ColumnSlice;
 use phpcassa\SystemManager;
 use phpcassa\Schema\StrategyClass;
@@ -13,105 +14,204 @@ class CassandraObject
     private $columnFamily = '';
     private $CFConnection;
     private $cassandra_connection = 'cassandra';
+    private $is_super = false;
 
-    public function __construct($cf,$connection_name='cassandra',$autopack_names=true,$autopack_values=true,$read_consistency_level=ConsistencyLevel::QUORUM,
-    $write_consistency_level=ConsistencyLevel::QUORUM,$buffer_size=1024)
+    public function __construct ($cf, $connection_name = 'cassandra', $autopack_names = true, $autopack_values = true,
+                                $read_consistency_level = ConsistencyLevel::QUORUM, $write_consistency_level = ConsistencyLevel::QUORUM, $buffer_size = 1024)
     {
         $_FLITE = Flite::Base();
+        $keyspace_description = @include ($_FLITE->GetConfig('site_root') . 'php-flite/cache/keyspace_description.php');
         $this->cassandra_connection = $connection_name;
         $this->columnFamily = $cf;
-        try { $this->CFConnection = new ColumnFamily($_FLITE->{$this->cassandra_connection}, $this->columnFamily,$autopack_names,$autopack_values,$read_consistency_level,$write_consistency_level,$buffer_size); }
-        catch (Exception $e){ $_FLITE->Exception('CassandraObject','__construct',$e); }
-        if(!$this->CFConnection) return false;
+
+        if ($keyspace_description && isset($keyspace_description->cf_defs))
+        {
+            foreach ($keyspace_description->cf_defs as $coldef)
+            {
+                if(isset($coldef->name) && $coldef->name == $this->columnFamily)
+                {
+                    $this->is_super = (isset($coldef->column_type) && $coldef->column_type == "Super");
+                }
+            }
+        }
+
+        try
+        {
+            if ($this->is_super)
+            {
+                $this->CFConnection = new SuperColumnFamily($_FLITE->{$this->cassandra_connection}, $this->columnFamily,
+                        $autopack_names, $autopack_values, $read_consistency_level, $write_consistency_level,
+                        $buffer_size);
+            }
+            else
+            {
+                $this->CFConnection = new ColumnFamily($_FLITE->{$this->cassandra_connection}, $this->columnFamily,
+                        $autopack_names, $autopack_values, $read_consistency_level, $write_consistency_level,
+                        $buffer_size);
+            }
+        }
+        catch (Exception $e)
+        {
+            $_FLITE->Exception('CassandraObject', '__construct', $e);
+        }
+        if (! $this->CFConnection) return false;
     }
 
-    public function GetData($key,$columns=null,$return_object=false)
+    public function GetData ($key, $columns = null, $return_object = false)
     {
         try
         {
-            $data = $this->CFConnection->get($key,null,$columns);
-            if(FC::count($columns) == 1 && $data) return $data[$columns[0]];
-            if($return_object && $data) return FC::array_to_object($data);
-            else if($data) return $data;
-            else throw new Exception('Key {'.$key.'} Not Found',404);
+            $data = $this->CFConnection->get($key, null, $columns);
+            if (FC::count($columns) == 1 && $data) return $data[$columns[0]];
+            if ($return_object && $data)
+                return FC::array_to_object($data);
+            else if ($data)
+                return $data;
+            else throw new Exception('Key {' . $key . '} Not Found', 404);
         }
-        catch (Exception $e){ $_FLITE = Flite::Base(); $_FLITE->Exception('CassandraObject','GetData',$e); return false; }
+        catch (Exception $e)
+        {
+            $_FLITE = Flite::Base();
+            $_FLITE->Exception('CassandraObject', 'GetData', $e);
+            return false;
+        }
     }
 
-    public function GetRange($start,$end,$count,$columns)
+    public function GetRange ($start, $end, $count, $columns)
     {
         try
         {
-            $data = $this->CFConnection->get_range($start,$end,$count,null,$columns);
-            if($data) return $data;
-            else throw new Exception('Range {'. $start.' - '. $end .'} Failed',404);
+            $data = $this->CFConnection->get_range($start, $end, $count, null, $columns);
+            if ($data)
+                return $data;
+            else throw new Exception('Range {' . $start . ' - ' . $end . '} Failed', 404);
         }
-        catch (Exception $e){ $_FLITE = Flite::Base(); $_FLITE->Exception('CassandraObject','GetRange',$e); return false; }
+        catch (Exception $e)
+        {
+            $_FLITE = Flite::Base();
+            $_FLITE->Exception('CassandraObject', 'GetRange', $e);
+            return false;
+        }
     }
 
-    public function SetData($key,$data,$ttl=null)
+    public function SetData ($key, $data, $ttl = null)
     {
         $insertdata = array();
         foreach ($data as $k => $v)
         {
             $insertdata[$k] = is_null($v) ? "" : $v;
         }
-        try { $this->CFConnection->insert($key,$insertdata,null,$ttl); }
-        catch (Exception $e){ $_FLITE = Flite::Base(); $_FLITE->Exception('CassandraObject','SetData',$e); return false; }
+        try
+        {
+            $this->CFConnection->insert($key, $insertdata, null, $ttl);
+        }
+        catch (Exception $e)
+        {
+            $_FLITE = Flite::Base();
+            $_FLITE->Exception('CassandraObject', 'SetData', $e);
+            return false;
+        }
         return true;
     }
 
-    public function GetColumns($key,$start_column = "",$end_column = "",$reverse_columns = false,$count=10,$return_object=false)
+    public function GetColumns ($key, $start_column = "", $end_column = "", $reverse_columns = false, $count = 10,
+                                $return_object = false)
     {
         try
         {
-            $columnslice = new SliceRange($start_column,$end_column,$count,$reverse_columns);
-            $data = $this->CFConnection->get($key,$columnslice);
-            if($return_object && $data) return FC::array_to_object($data);
-            else if($data) return $data;
-            else throw new Exception('Key {'.$key.'} Not Found',404);
+            $columnslice = new ColumnSlice($start_column, $end_column, $count, $reverse_columns);
+            if ($this->is_super)
+            {
+                $data = $this->CFConnection->get($key, $columnslice);
+            }
+            else
+            {
+                $data = $this->CFConnection->get($key, $columnslice);
+            }
+            if ($return_object && $data)
+                return FC::array_to_object($data);
+            else if ($data)
+                return $data;
+            else throw new Exception('Key {' . $key . '} Not Found', 404);
         }
-        catch (Exception $e){ $_FLITE = Flite::Base(); $_FLITE->Exception('CassandraObject','GetColumns',$e); return false; }
+        catch (Exception $e)
+        {
+            $_FLITE = Flite::Base();
+            $_FLITE->Exception('CassandraObject', 'GetColumns', $e);
+            return false;
+        }
     }
 
-    public function Delete($key,$columns=null)
-    {
-        try { $this->CFConnection->remove($key,$columns); }
-        catch (Exception $e){ $_FLITE = Flite::Base(); $_FLITE->Exception('CassandraObject','Delete',$e); return false; }
-        return true;
-    }
-
-    public function GetMulti($keys,$columns=null,$column_start="",$column_finish="",$reverse_order=false,$column_count=100)
+    public function Delete ($key, $columns = null)
     {
         try
         {
-            $columnslice = new SliceRange($column_start,$column_finish,$column_count,$reverse_order);
-            $data = $this->CFConnection->multiget($keys,$columnslice,$columns);
-            if($data) return $data;
-            else throw new Exception('MultiGet Failed',404);
+            $this->CFConnection->remove($key, $columns);
         }
-        catch (Exception $e){ $_FLITE = Flite::Base(); $_FLITE->Exception('CassandraObject','GetMulti',$e); return false; }
-    }
-
-    public function Increment($key,$column,$increase_by=1)
-    {
-        try { $this->CFConnection->add($key,$column,$increase_by,null,ConsistencyLevel::ONE); }
-        catch (Exception $e){ $_FLITE = Flite::Base(); $_FLITE->Exception('CassandraObject','Increment',$e); return false; }
+        catch (Exception $e)
+        {
+            $_FLITE = Flite::Base();
+            $_FLITE->Exception('CassandraObject', 'Delete', $e);
+            return false;
+        }
         return true;
     }
 
-    public function Decrement($key,$column,$reduce_by=1)
+    public function GetMulti ($keys, $columns = null, $column_start = "", $column_finish = "", $reverse_order = false,
+                            $column_count = 100)
     {
-        if($reduce_by > 0) $reduce_by = -1 * $reduce_by;
-        try { $this->CFConnection->add($key,$column,$increase_by,null,ConsistencyLevel::ONE); }
-        catch (Exception $e){ $_FLITE = Flite::Base(); $_FLITE->Exception('CassandraObject','Decrement',$e); return false; }
+        try
+        {
+            $columnslice = new ColumnSlice($column_start, $column_finish, $column_count, $reverse_order);
+            $data = $this->CFConnection->multiget($keys, $columnslice, $columns);
+            if ($data)
+                return $data;
+            else throw new Exception('MultiGet Failed', 404);
+        }
+        catch (Exception $e)
+        {
+            $_FLITE = Flite::Base();
+            $_FLITE->Exception('CassandraObject', 'GetMulti', $e);
+            return false;
+        }
+    }
+
+    public function Increment ($key, $column, $increase_by = 1)
+    {
+        try
+        {
+            $this->CFConnection->add($key, $column, $increase_by, null, ConsistencyLevel::ONE);
+        }
+        catch (Exception $e)
+        {
+            $_FLITE = Flite::Base();
+            $_FLITE->Exception('CassandraObject', 'Increment', $e);
+            return false;
+        }
         return true;
     }
 
-    public function __call($method, $args)
+    public function Decrement ($key, $column, $reduce_by = 1)
     {
-        if($method != 'remove') FC::error_report("Direct PhpCassa Call",array("Method" => $method, "Args" => $args));
-        if(method_exists($this->CFConnection,$method)) return call_user_func_array(array($this->CFConnection,$method),$args);
-        throw new ErrorException ('Call to Undefined Method/Class Function', 0, E_ERROR);
+        if ($reduce_by > 0) $reduce_by = - 1 * $reduce_by;
+        try
+        {
+            $this->CFConnection->add($key, $column, $increase_by, null, ConsistencyLevel::ONE);
+        }
+        catch (Exception $e)
+        {
+            $_FLITE = Flite::Base();
+            $_FLITE->Exception('CassandraObject', 'Decrement', $e);
+            return false;
+        }
+        return true;
+    }
+
+    public function __call ($method, $args)
+    {
+        if ($method != 'remove') FC::error_report("Direct PhpCassa Call", array("Method" => $method,"Args" => $args));
+        if (method_exists($this->CFConnection, $method)) return call_user_func_array(array($this->CFConnection,$method),
+                $args);
+        throw new ErrorException('Call to Undefined Method/Class Function', 0, E_ERROR);
     }
 }
