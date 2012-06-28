735820011cb65bcfa3391d0c361568916711c5ec
brooke.bryan <brooke.bryan@7a0bffa2-0bca-f930-4f03-409d7255b43b>
2012-06-28 09:09:38 +0000

    Validate_Email now checks the host exists, can be disabled with the 2nd param
    git-svn-id: https://php-flite.googlecode.com/svn/trunk@159 7a0bffa2-0bca-f930-4f03-409d7255b43b

--- php-flite/core.lib/common.php
+++ php-flite/core.lib/common.php
@@ -124,9 +124,15 @@ class FC
         return false;
     }
 
-    public static function validate_email($email)
+    public static function validate_email($email,$dns=true)
     {
-        return preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,4})$/", $email);
+        if ( preg_match("/^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$/", $email) )
+        {
+            if(!$dns) return true;
+            list($usr,$host) = explode('@',$email,2);
+            if(checkdnsrr($host) || FLITE_ENV == 'dev') return true;
+        }
+        return false;
     }
 
     public static function ascii_encode_email($str_email,$str_display='',$bln_create_link=true) {
