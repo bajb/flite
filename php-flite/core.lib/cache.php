bf552b8c7f5172f6385bcb1ef00ce42e6c8aaee6
evans.gareth0@gmail.com <evans.gareth0@gmail.com@7a0bffa2-0bca-f930-4f03-409d7255b43b>
2012-07-20 09:59:33 +0000

    Changed Cache file to check if content has changed, and uncoupled some bits
    git-svn-id: https://php-flite.googlecode.com/svn/trunk@182 7a0bffa2-0bca-f930-4f03-409d7255b43b

--- php-flite/core.lib/cache.php
+++ php-flite/core.lib/cache.php
@@ -1,25 +1,53 @@
 <?php
 class FliteCache extends FliteConfig
 {
+    const CACHE_DIR = '/cache/';
+    const CACHE_KEY = 'fliteconfig_';
+
     public function GetCache($file)
     {
-        $key = 'fliteconfig_'.md5($file);
+        $key = self::CACHE_KEY.md5($file);
         $cache = $this->GetConfig($key, null);
         if(is_null($cache))
         {
-            $_FLITE = Flite::Base();
-            if(file_exists(FLITE_DIR . '/cache/'.$file))
-            {
-                $cache = file_get_contents(FLITE_DIR . '/cache/'.$file);
-                $this->SetConfig($key, $cache);
-            }
+            $cache = $this->_LoadFile($file);
+            if(!is_null($cache)) $this->SetConfig($key, $cache);
         }
-        return is_null($cache) ? false : unserialize($cache);
+        return is_null($cache) ? false : $this->_UnserializeDate($cache);
     }
 
     public function CreateCache($file,$data)
     {
-        $_FLITE = Flite::Base();
-        return file_put_contents(FLITE_DIR . '/cache/'.$file, serialize($data));
+        if(!$this->_FileChanged($file, $data)) return null;
+        return file_put_contents(FLITE_DIR . self::CACHE_DIR . $file, $this->_SerializeData($data));
+    }
+
+    private function _LoadFile($file)
+    {
+        if(file_exists(FLITE_DIR . self::CACHE_DIR . $file))
+        {
+            return file_get_contents(FLITE_DIR . self::CACHE_DIR . $file);
+        }
+        return null;
+    }
+
+    private function _FileChanged($file, $data)
+    {
+        $cache = $this->_LoadFile($file);
+        if(!is_null($cache))
+        {
+            return md5($cache) !== md5($this->_SerializeData($data));
+        }
+        return true;
+    }
+
+    private function _SerializeData($data)
+    {
+        return serialize($data);
+    }
+
+    private function _UnserializeDate($data)
+    {
+        return unserialize($data);
     }
 }
