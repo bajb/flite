6a0082011849e5b46a208e8431b441706cacfe10
Jay Francis <jay.francis@jdiuk.com>
2012-11-22 15:22:36 +0000

    Added Flite_Filter::URI() for creating URI from a string

--- php-flite/lib/flite/filter.php
+++ php-flite/lib/flite/filter.php
@@ -9,101 +9,112 @@ class Flite_Filter
 {
 
 
-    public static function SingleSpaces($string)
+  public function URI($string)
+  {
+    $string = strtolower($string);
+    $string = preg_replace("/[^A-Za-z0-9 ]/", '', $string);
+    $string = trim($string);
+    $string = Flite_Filter::SingleSpaces($string);
+    $string = str_replace(' ', '-', $string);
+    return $string;
+  }
+
+  public static function SingleSpaces($string)
+  {
+    return preg_replace('!\s+!', ' ', $string);
+  }
+
+  public static function Email($email)
+  {
+    return strtolower(trim($email));
+  }
+
+  public static function Trim($string, $charlist = null)
+  {
+    return trim($string, $charlist);
+  }
+
+  public static function LeftTrim($string, $charlist = null)
+  {
+    return ltrim($string, $charlist);
+  }
+
+  public static function RightTrim($string, $charlist = null)
+  {
+    return rtrim($string, $charlist);
+  }
+
+  public static function Lower($string)
+  {
+    return strtolower($string);
+  }
+
+  public static function Upper($string)
+  {
+    return strtoupper($string);
+  }
+
+  public static function UpperWords($string)
+  {
+    return ucwords(strtolower($string));
+  }
+
+  public static function Clean($string)
+  {
+    return trim(strip_tags($string));
+  }
+
+  public static function Boolean($string)
+  {
+    return in_array($string, array('true', '1', 1, true), true);
+  }
+
+  public static function Int($string)
+  {
+    return intval($string);
+  }
+
+  public static function Float($string)
+  {
+    return floatval($string);
+  }
+
+  public static function Arr($string)
+  {
+    if(is_array($string)) return $string;
+    if(is_object($string)) return FC::object_to_array($string);
+    if(stristr($string, ',')) return explode(',', $string);
+    else return array($string);
+  }
+
+  /**
+   * Returns a name object
+   *
+   * @param $full_name
+   * @return stdClass
+   */
+  public static function SplitName($full_name)
+  {
+    $full_name        = preg_replace('!\s+!', ' ', $full_name); // Make multiple spaces single
+    $name             = new stdClass();
+    $parts            = explode(' ', trim($full_name));
+    $name->first_name = $name->middle_name = $name->last_name = '';
+    switch(count($parts))
     {
-        return preg_replace('!\s+!', ' ', $string);
+      case 1:
+        $name->first_name = $parts[0];
+        break;
+      case 2:
+        $name->first_name = $parts[0];
+        $name->last_name  = $parts[1];
+        break;
+      default:
+        $name->first_name  = array_shift($parts);
+        $name->last_name   = array_pop($parts);
+        $name->middle_name = implode(' ', $parts);
+        break;
     }
 
-    public static function Email($email)
-    {
-        return strtolower(trim($email));
-    }
-
-    public static function Trim($string, $charlist = null)
-    {
-        return trim($string, $charlist);
-    }
-
-    public static function LeftTrim($string, $charlist = null)
-    {
-        return ltrim($string, $charlist);
-    }
-
-    public static function RightTrim($string, $charlist = null)
-    {
-        return rtrim($string, $charlist);
-    }
-
-    public static function Lower($string)
-    {
-        return strtolower($string);
-    }
-
-    public static function Upper($string)
-    {
-        return strtoupper($string);
-    }
-
-    public static function UpperWords($string)
-    {
-        return ucwords(strtolower($string));
-    }
-
-    public static function Clean($string)
-    {
-        return trim(strip_tags($string));
-    }
-
-    public static function Boolean($string)
-    {
-        return in_array($string, array('true', '1', 1, true), true);
-    }
-
-    public static function Int($string)
-    {
-        return intval($string);
-    }
-
-    public static function Float($string)
-    {
-        return floatval($string);
-    }
-
-    public static function Arr($string)
-    {
-        if(is_array($string)) return $string;
-        if(is_object($string)) return FC::object_to_array($string);
-        if(stristr($string, ',')) return explode(',', $string);
-        else return array($string);
-    }
-
-    /**
-     * Returns a name object
-     * @param $full_name
-     * @return stdClass
-     */
-    public static function SplitName($full_name)
-    {
-        $full_name = preg_replace('!\s+!', ' ', $full_name); // Make multiple spaces single
-        $name             = new stdClass();
-        $parts            = explode(' ', trim($full_name));
-        $name->first_name = $name->middle_name = $name->last_name = '';
-        switch(count($parts))
-        {
-            case 1:
-                $name->first_name = $parts[0];
-                break;
-            case 2:
-                $name->first_name = $parts[0];
-                $name->last_name  = $parts[1];
-                break;
-            default:
-                $name->first_name  = array_shift($parts);
-                $name->last_name   = array_pop($parts);
-                $name->middle_name = implode(' ', $parts);
-                break;
-        }
-
-        return $name;
-    }
+    return $name;
+  }
 }
